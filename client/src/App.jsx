547ed4baf26e99463f708200346fb1003b5c9900
Suraj Roy <surajroy7430@gmail.com>
2026-08-15 20:55:57 +0530

    Increase max width of main container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div className="min-h-screen p-6 max-w-5xl mx-auto w-full">
+    <div className="min-h-screen p-6 max-w-6xl mx-auto w-full">
       <AppRoutes />
 
       <Toaster position="top-right" richColors closeButton />
